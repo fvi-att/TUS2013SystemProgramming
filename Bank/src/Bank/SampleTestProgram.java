9d27178bedd703e9f67d4c6a139745619e7acf4d
fviAtt <jshimizujp@gmail.com>
2013-09-27 16:11:41 +0900

    add UML File

---
+++ Bank/src/Bank/SampleTestProgram.java
@@ -0,0 +1,17 @@
+package Bank;
+
+public class SampleTestProgram {
+
+	/**
+	 * @param args
+	 */
+	
+	public static int Plus(int a, int b){
+		return a;
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
